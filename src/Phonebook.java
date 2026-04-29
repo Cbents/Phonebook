cb6a42558747ae84e153e452bbd43a440e391899
benf <jedidoh21@gmail.com>
2026-04-29 13:27:55 -0600

    Phonebook

--- src/Phonebook.java
+++ src/Phonebook.java
@@ -1,3 +1,47 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Phonebook {
     //hello?
+
+
+    public void readFromFile(){
+        try (Scanner reader = new Scanner(new File("contacts.txt"))){
+            while (reader.hasNextLine()){
+                String next = reader.nextLine();
+                try {
+                    addContact(next);
+                }
+                catch (ArrayIndexOutOfBoundsException e){
+                    System.out.print("Not a valid contact.");
+                }
+
+            }
+        }
+        catch (FileNotFoundException e){
+            System.out.println("File not found");
+        }
+    }
+
+    public void saveToFile(){
+        String addToFile = "";
+        try (FileWriter writer = new FileWriter("contacts.txt")){
+            ArrayList<String> contacts = getContacts();
+            for (String s : contacts) {
+                addToFile += s + "\n";
+            }
+            writer.write(addToFile);
+            System.out.println("Saved to file");
+        }
+
+        catch (IOException e){
+            System.out.println("An error occurred while writing" + " to the file: " + e.getMessage());
+        }
+    }
 }
+
+
