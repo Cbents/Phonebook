526fa5ae5870fdb823d8917598e03617245f3b77
Asublime01 <99103839+Asublime01@users.noreply.github.com>
2026-04-29 14:13:07 -0600

    AddContact & ViewAll created

--- src/Phonebook.java
+++ src/Phonebook.java
@@ -2,12 +2,13 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Phonebook {
-    //hello?
-    //testing?
+
+    ArrayList<ContactCard> phonebook = new ArrayList<>();
 
     public void readFromFile(){
         try (Scanner reader = new Scanner(new File("contacts.txt"))){
@@ -40,6 +41,19 @@ public class Phonebook {
 
         catch (IOException e){
             System.out.println("An error occurred while writing" + " to the file: " + e.getMessage());
+        }
+    }
+
+    public void addContact(String name, String number){
+        ContactCard newContact = new ContactCard(name, number);
+        phonebook.add(newContact);
+
+    }
+
+    public void viewAllContacts(){
+        for (int i = 0; i < phonebook.size(); i++){
+            String forOut = String.format("%d: %s", i, phonebook.get(i).getName());
+            System.out.println(forOut);
         }
     }
 }
